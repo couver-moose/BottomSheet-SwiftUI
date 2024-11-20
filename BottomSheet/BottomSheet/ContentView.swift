4b5efb172b139ad199d9e26ba4f2230f0d92fb22
Dario Daßler <dario@weloveapps.de>
2024-11-20 10:03:03 +0100

    implememt mapview with bottom sheet

--- BottomSheet/BottomSheet/ContentView.swift
+++ BottomSheet/BottomSheet/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var tapped = false
+    @State private var sheet = true
+    
+    let cities = [
+        "Berlin",
+        "München",
+        "Hamburg",
+        "Köln",
+        "Frankfurt am Main",
+        "Stuttgart",
+        "Düsseldorf",
+        "Leipzig",
+        "Dortmund",
+        "Essen",
+        "Bremen",
+        "Dresden",
+        "Hannover",
+        "Nürnberg",
+        "Duisburg",
+        "Bochum",
+        "Wuppertal",
+        "Bielefeld",
+        "Bonn",
+        "Mannheim",
+        "Karlsruhe",
+        "Wiesbaden",
+        "Augsburg",
+        "Münster",
+        "Gelsenkirchen",
+        "Aachen",
+        "Braunschweig",
+        "Kiel",
+        "Magdeburg",
+        "Freiburg im Breisgau",
+        "Lübeck",
+        "Erfurt",
+        "Rostock",
+        "Mainz",
+        "Kassel",
+        "Saarbrücken",
+        "Potsdam",
+        "Halle (Saale)",
+        "Koblenz",
+        "Oldenburg",
+        "Osnabrück"
+    ]
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        Map()
+        .sheet(isPresented: $sheet) {
+            VStack {
+                List(cities, id: \.self) { city in
+                    Text(city)
+                        .listRowBackground(Color.clear)
+                }.scrollContentBackground(.hidden)
+            }
+            .presentationDetents([.height(200), .medium]) //Fix punkte für das Sheet (mindestens zwei sonst funktioniert .presentationBackgroundInteraction(.enabled) nicht
+            .presentationBackground(.ultraThinMaterial) //Hintergrund des Sheets
+            .presentationCornerRadius(30) //Radius der Ecken
+            .presentationBackgroundInteraction(.enabled) //Macht das Sheet zum BottomSHeet so kann man mit dem Hintergrund View interagieren
+            .presentationDragIndicator(.visible) //Einstellung für den Drag indicator
+            .interactiveDismissDisabled(true) //damit schließt sich das Sheet nicht durch nach unten wischen
         }
-        .padding()
+
+       
+        
     }
 }
 
