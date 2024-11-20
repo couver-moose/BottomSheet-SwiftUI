e8107735ad1ea382700e12e4b38cffaeeb599d22
Dario Daßler <dario@weloveapps.de>
2024-11-20 09:04:18 +0100

    creat xcode project

---
+++ BottomSheet/BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Dario Daßler on 20.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
